f0e793f9baea066cfbc26a35048d0abdbf9c54e7
Skerter <geopank55@gmail.com>
2025-03-11 23:13:58 +0300

    сосал!!!

---
+++ app.py
@@ -0,0 +1,57 @@
+import dotenv
+import os
+import logging
+import asyncio
+from rich.logging import RichHandler
+import kivy
+kivy.require('2.3.1')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+dotenv.load_dotenv()
+DEV_MODE = os.getenv('DEV_MODE')
+DEV_MODE = True if DEV_MODE == "YES" else False
+
+log_folder = "log"
+os.makedirs(log_folder, exist_ok=True)
+log_file = os.path.join(log_folder, "main.log")
+
+tmp_folder = "./tmp"
+os.makedirs(tmp_folder, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[
+        RichHandler(),
+        logging.FileHandler(log_file, encoding="utf-8")
+    ]
+)
+logger = logging.getLogger("rich")
+
+
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+class MatCoinApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+
+if __name__ == '__main__':
+    MatCoinApp().run()
