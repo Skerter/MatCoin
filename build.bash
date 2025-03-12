abafcea83bf565fd1ec4f26b6001182849a2e83a
builder <builder>
2025-03-13 01:53:47 +0300

    Чето наебланил

--- build.bash
+++ build.bash
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-rm -rf bin
+if [ ! -d ".venv" ]; then
+    echo "Виртуальное окружение не найдено, создаю новое..."
+    python3 -m venv .venv
+else
+    echo "Виртуальное окружение найдено, активирую его..."
+fi
 
-python3 -m venv .venv
 . .venv/bin/activate
+
+echo "Устанавливаю зависимости из requirements.txt..."
 pip3 install -r requirements.txt
 
+echo "Удаляю старые сборки в bin..."
+rm -rf bin
+
+echo "Запускаю сборку с Buildozer..."
 buildozer android debug
